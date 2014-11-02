c2073c3535d5b566bb6332dec7f9cf8e01f3500c
寺内 信夫 <tenobu@gmail.com>
2014-11-02 22:32:01 +0900

    地図、GPSの初期化

---
+++ Shiro_100/RankingViewController.h
@@ -0,0 +1,13 @@
+//
+//  RankingViewController.h
+//  Shiro_100
+//
+//  Created by 寺内 信夫 on 2014/11/02.
+//  Copyright (c) 2014年 寺内 信夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RankingViewController : UITableViewController < UINavigationControllerDelegate, UITableViewDelegate >
+
+@end
---
+++ Shiro_100/RankingViewController.m
@@ -0,0 +1,37 @@
+//
+//  RankingViewController.m
+//  Shiro_100
+//
+//  Created by 寺内 信夫 on 2014/11/02.
+//  Copyright (c) 2014年 寺内 信夫. All rights reserved.
+//
+
+#import "RankingViewController.h"
+
+@interface RankingViewController ()
+
+@end
+
+@implementation RankingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
